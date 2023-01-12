4935004a203fa4f6a1db3da28b1c220d5eef707e
Claire Lovisa <lovisa.claire@gmail.com>
2023-01-12 15:15:29 +0100

    Fix loading table message

--- app/controllers/administrative-units/index.js
+++ app/controllers/administrative-units/index.js
@@ -47,10 +47,6 @@ export default class AdministrativeUnitsIndexController extends Controller {
     );
   }
 
-  get showTableLoader() {
-    return this.isLoading && !this.hasPreviousData;
-  }
-
   get hasNoResults() {
     return (
       this.model.loadAdministrativeUnitsTaskInstance.isFinished &&
