b9ea91e5d3e47e2689f08ea6022392602c651219
Claire Lovisa <lovisa.claire@gmail.com>
2023-06-09 12:37:58 +0200

    Add line breaks

--- app/serializers/administrative-unit.js
+++ app/serializers/administrative-unit.js
@@ -1,4 +1,5 @@
 import ApplicationSerializer from './application';
+
 export default class AdministrativeUnitSerializer extends ApplicationSerializer {
   attrs = {
     subOrganizations: { serialize: true },
