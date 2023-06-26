1a3fc0be945e86a358de745964339f06842d8edc
Claire Lovisa <lovisa.claire@gmail.com>
2023-06-26 17:35:35 +0200

    Add stylelint-config-standard-scss

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -2,4 +2,12 @@
 
 module.exports = {
   extends: ['stylelint-config-standard', 'stylelint-prettier/recommended'],
+  rules: {
+    'selector-class-pattern': null, // This enforces kebab-case but we use BEM which isn't compatible
+    'at-rule-no-unknown': null,
+    'no-descending-specificity': null,
+    'annotation-no-unknown': null,
+    'import-notation': null,
+    'function-no-unknown': null,
+  },
 };
