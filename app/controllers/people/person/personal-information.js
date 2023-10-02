81bc90f57c608c5b124186f1c006637f5b8b6f88
Nordine Bittich <3816305+nbittich@users.noreply.github.com>
2023-10-02 16:48:37 +0200

    Feature/privacy (#9)

--- app/controllers/people/person/personal-information.js
+++ app/controllers/people/person/personal-information.js
@@ -1,14 +1,5 @@
 import Controller from '@ember/controller';
-import { service } from '@ember/service';
 
 export default class PeoplePersonPersonalInformationController extends Controller {
-  @service router;
-
-  get currentURL() {
-    return this.router.currentURL;
-  }
-
-  get nationalities() {
-    return this.model.nationalities.map((n) => n.nationalityLabel).join(', ');
-  }
+  reset() {}
 }
---
+++ app/controllers/people/person/personal-information/index.js
@@ -0,0 +1,19 @@
+import Controller from '@ember/controller';
+import { inject as service } from '@ember/service';
+
+export default class PeoplePersonPersonalInformationIndexController extends Controller {
+  @service router;
+
+  get currentURL() {
+    return this.router.currentURL;
+  }
+  get nationalities() {
+    return this.model.requestSensitiveInformation.nationalities
+      .map((n) => n.nationalityLabel)
+      .join(', ');
+  }
+
+  get positionsCantBeCreatedOrEdited() {
+    return new Date() >= new Date('2023-02-01');
+  }
+}
